be1116879f0eb0df5e9c36eec270b92ac572235d
csbuchert <csaba.buchert@nng.com>
2021-10-13 15:47:25 +0200

    set production host

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://udacitywebhostproject-env.eba-zt7vwbnb.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
